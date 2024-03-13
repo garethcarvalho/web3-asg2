2cf4c552738c6a23f93c4de3022dae6aae4fcd9d
Gareth Carvalho <garethcarva@gmail.com>
2024-03-13 01:29:45 -0600

    edited header bar

--- src/components/HeaderBar.jsx
+++ src/components/HeaderBar.jsx
@@ -4,16 +4,16 @@ const HeaderBar = ({seasons, currentSeason, setSeason, onLogout}) => {
     }
 
     return (
-        <div>
-            <div>
-                <label>Season</label>
+        <div className="flex justify-between py-4 m-2 bg-blue-300">
+            <div className="ml-8">
+                <label className="mr-2">Season</label>
                 <select name="season" value={currentSeason} onChange={onSeasonChanged}>
                     {seasons.map(s => <option value={s} key={s}>{s}</option>)}
                 </select>
             </div>
-            <h2>F1 Dashboard</h2>
-            <div>
-                <button>Favourites</button>
+            <h2 className="font-bold text-lg">F1 Dashboard</h2>
+            <div className="mr-8">
+                <button className="">Favourites</button>
                 <button>About</button>
                 <button onClick={onLogout}>Logout</button>
             </div>
