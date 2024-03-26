c33663e9de03746007609bcf2cd91fdefc18fc1a
garethcarvalho <gcarv023@mtroyal.ca>
2024-03-26 12:35:50 -0600

    Started on Modal Windows

--- src/components/UserFormView.jsx
+++ src/components/UserFormView.jsx
@@ -1,8 +1,12 @@
+import Modal from "./Modal";
 import UserForm from "./UserForm";
 
 const UserFormView = ({onLogin}) => {
     return (
         <div className="bg-blue-200">
+            <Modal>
+                <div>This is a modal!</div>
+            </Modal>
             <UserForm onLogin={onLogin} />
             <div className="w-screen align-middle text-center">Photo by Name Nameswell</div>
         </div>
